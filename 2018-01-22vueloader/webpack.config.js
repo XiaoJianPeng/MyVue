6123e3612c79259d0a3d92cb94ba3f12349aba2d
xiaojianpeng <xiao8901@foxmail.com>
2018-01-22 18:07:23 +0800

    vue-loader 练习

---
+++ 2018-01-22vueloader/webpack.config.js
@@ -0,0 +1,31 @@
+const  webpack = require('webpack');
+module.exports={
+    entry:'./main.js',
+
+    output:{
+        path:__dirname,
+        filename:'build.js'
+    },
+    
+    module:{
+        loaders:[
+            {test:/\.vue$/,loader:'vue-loader'},
+            {test:/\.js$/,loader:'babel-loader',exclude:/node_modules/}
+        ]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                babel: {
+                    presets:['es2015'],
+                    plugins:['transform-runtime']
+                }
+            }
+        })
+    ]
+    // babel:{
+    //     presets:['es2015'],
+    //     plugins:['transform-runtime']
+    // }
+    
+}
